0aea746ee965f250553116bfbba10c2a9eca3ce3
Julia O'Neill <jmoneill@cisc375.cis.udel.edu>
2025-04-30 17:04:12 +0000

    pushing jest tests?

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	moduleNameMapper: {
-		'^@root/(.*)$': '<rootDir>/$1',
-		'^@lib/(.*)$': '<rootDir>/src/lib/$1',
-		'^@pieces/(.*)$': '<rootDir>/src/pieces/$1',
+	transform: {
+	  '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.json' }],
 	},
-	globals: {
-		'ts-jest': {
-		  tsconfig: './tsconfig.json',
-		},
-	  },
+	moduleNameMapper: {
+	  '^@root/(.*)$': '<rootDir>/$1',
+	  '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+	  '^@pieces/(.*)$': '<rootDir>/src/pieces/$1',
+	},
   };
+  
    
