2376e533cce701d1e92516ad7536ec4464d42f6b
Wesley <wesleysimpson02@gmail.com>
2025-10-24 01:54:38 -0400

    Add passing Jest tests and update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,23 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-	testEnvironment: "node",
-	transform: {
-		...tsJestTransformCfg,
-	},
-	// This is the part you need to add
-	moduleNameMapper: {
-		"^@lib/(.*)$": "<rootDir>/src/lib/$1",
-		"^@root/(.*)$": "<rootDir>/$1",
-	},
+    testEnvironment: "node",
+
+    // Only look for tests in the source folder
+    roots: ["<rootDir>/src"],
+
+    // Ignore compiled files
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
+
+    transform: {
+        ...tsJestTransformCfg,
+    },
+
+    moduleNameMapper: {
+        "^@lib/(.*)$": "<rootDir>/src/lib/$1",
+        "^@root/(.*)$": "<rootDir>/$1",
+    },
+
+    // Optional: restrict Jest to only test files ending in .test.ts or .spec.ts
+    // testMatch: ["**/__test__/**/*.test.[jt]s?(x)"],
 };
