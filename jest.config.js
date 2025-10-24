0e7cc28b376e2073267fbf448dc52a7fd4267671
Kevin Lundin <kevinlundin@comcast.net>
2025-10-24 12:52:33 -0400

    5 stafftests done

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,22 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-	testEnvironment: "node",
-	transform: {
-		...tsJestTransformCfg,
-	},
-	// This is the part you need to add
-	moduleNameMapper: {
-		"^@lib/(.*)$": "<rootDir>/src/lib/$1",
-		"^@root/(.*)$": "<rootDir>/$1",
-	},
+    testEnvironment: "node",
+
+    // Only look for tests in the source folder
+    roots: ["<rootDir>/src"],
+
+    // Ignore compiled files
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
+
+    transform: {
+        ...tsJestTransformCfg,
+    },
+
+	resetModules: false,
+
+    moduleNameMapper: {
+        "^@lib/(.*)$": "<rootDir>/src/lib/$1",
+        "^@root/(.*)$": "<rootDir>/$1",
+    },
 };
