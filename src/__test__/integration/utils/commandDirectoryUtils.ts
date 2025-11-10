746e97dbcdf3834ea2d41aeb3d60f7106c8076c6
Jared Miller <jaredmiller219@gmail.com>
2025-11-10 12:38:55 -0500

    added spacing in utils file for better readability

--- src/__test__/integration/utils/commandDirectoryUtils.ts
+++ src/__test__/integration/utils/commandDirectoryUtils.ts
@@ -9,6 +9,12 @@ export function getCommandNames(relativeDir: string): string[] {
 	const integrationRoot = path.resolve(__dirname, '..');
 	const commandsDir = path.resolve(integrationRoot, relativeDir);
 	return readdirSync(commandsDir, { withFileTypes: true })
-		.filter(entry => entry.isFile() && /\.(ts|js)$/.test(entry.name) && !entry.name.endsWith('.d.ts'))
-		.map(entry => entry.name.replace(/\.(ts|js)$/, ''));
+		.filter(entry => 
+			entry.isFile()
+			&& /\.(ts|js)$/.test(entry.name) 
+			&& !entry.name.endsWith('.d.ts'))
+		
+		.map(entry => 
+			entry.name.replace(/\.(ts|js)$/, '')
+		);
 }
